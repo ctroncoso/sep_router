f0459e8a599d5a8f1acf3070412057615df78136
Carlos Troncoso Phillips <schmickcl@gmail.com>
2012-01-06 12:29:52 -0300

    added assets and prestaciones views

---
+++ app/controllers/prestaciones_controller.rb
@@ -0,0 +1,83 @@
+class PrestacionesController < ApplicationController
+  # GET /prestacions
+  # GET /prestacions.json
+  def index
+    @prestacions = Prestacion.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @prestacions }
+    end
+  end
+
+  # GET /prestacions/1
+  # GET /prestacions/1.json
+  def show
+    @prestacion = Prestacion.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @prestacion }
+    end
+  end
+
+  # GET /prestacions/new
+  # GET /prestacions/new.json
+  def new
+    @prestacion = Prestacion.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @prestacion }
+    end
+  end
+
+  # GET /prestacions/1/edit
+  def edit
+    @prestacion = Prestacion.find(params[:id])
+  end
+
+  # POST /prestacions
+  # POST /prestacions.json
+  def create
+    @prestacion = Prestacion.new(params[:prestacion])
+
+    respond_to do |format|
+      if @prestacion.save
+        format.html { redirect_to @prestacion, notice: 'Prestacion was successfully created.' }
+        format.json { render json: @prestacion, status: :created, location: @prestacion }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @prestacion.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /prestacions/1
+  # PUT /prestacions/1.json
+  def update
+    @prestacion = Prestacion.find(params[:id])
+
+    respond_to do |format|
+      if @prestacion.update_attributes(params[:prestacion])
+        format.html { redirect_to @prestacion, notice: 'Prestacion was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @prestacion.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /prestacions/1
+  # DELETE /prestacions/1.json
+  def destroy
+    @prestacion = Prestacion.find(params[:id])
+    @prestacion.destroy
+
+    respond_to do |format|
+      format.html { redirect_to prestacions_url }
+      format.json { head :ok }
+    end
+  end
+end
