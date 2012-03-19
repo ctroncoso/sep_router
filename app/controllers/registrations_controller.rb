1cc2071bec33e1852421cce1dde784fda0756bf8
Carlos Troncoso Phillips <schmickcl@gmail.com>
2012-03-19 00:31:11 -0300

    hotfix: registration needed skip :create and :new

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-  skip_filter :require_no_authentication, :only => [:new]
+  skip_filter :require_no_authentication, :only => [:new, :create]
   def new
     unless current_user.try(:admin)
       flash[:notice] = "Solo un administrador puede crear nuevos usuarios"
@@ -7,5 +7,9 @@ class RegistrationsController < Devise::RegistrationsController
     else
       super
     end
+  end
+
+  def create
+    super
   end
 end
