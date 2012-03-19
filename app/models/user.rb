3c87bd50a4db1a8ace64a2238f1895850918d280
Carlos Troncoso <schmickcl@gmail.com>
2012-03-19 00:46:47 -0300

    Usuarios puede cambiar su contraseña

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable, :rememberable,  and :omniauthable
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :rememberable,  and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :validatable
+         :recoverable, :trackable, :validatable,
+         :timeoutable, :timeout_in => 25.minute
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
